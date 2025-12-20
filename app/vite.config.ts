4ded924b495e46ee7398a5430ac8a92db1cc1582
mohtashimnawaz <mohtashim3nawaz@gmail.com>
2025-12-21 04:40:20 +0530

    dep 2

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,3 +1,8 @@
+  build: {
+    rollupOptions: {
+      external: ['@solana/spl-token'],
+    },
+  },
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
