5ef1a447f2140d36bd3cbb9ffe0ce5a796347fee
mohtashimnawaz <mohtashim3nawaz@gmail.com>
2025-12-21 17:01:25 +0530

    dep 7

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import process from 'process';
 
 export default defineConfig({
   plugins: [react()],
@@ -9,7 +10,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
       'stream': 'stream-browserify',
       'buffer': 'buffer',
-      'process': 'process/browser.js',
+      'process': require.resolve('process/browser.js'),
     },
   },
   define: {
